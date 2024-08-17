7462f0b205d898a60db725d9fb8f118894f4d441
nitish <kumarnitish27212@gmail.com>
2024-08-18 02:38:26 +0530

    responsive

--- public/js/script.js
+++ public/js/script.js
@@ -18,29 +18,8 @@ Array.from(forms).forEach(form => {
 })
 })()
 
-//signup page
 
-
-      
-// $(document).ready(function () {
-//   $('#signupForm').on('submit', function (event) {
-//       event.preventDefault();
-      
-//       const password = $('#password').val();
-//       const confirmPassword = $('#confirmPassword').val();
-//       const passwordPattern = /^(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
-
-//       if (!passwordPattern.test(password)) {
-//           $('#message').html('<div class="alert alert-danger">Password must be at least 8 characters long, contain a number and a special character.</div>');
-//           return;
-//       }
-
-//       if (password !== confirmPassword) {
-//           $('#message').html('<div class="alert alert-danger">Passwords do not match.</div>');
-//           return;
-//       }
-
-//       // If form is valid, you can submit it
-//       this.submit();
-//   });
-// });
+function toggleMenu() {
+  const menuLinks = document.querySelector('.menu-links');
+  menuLinks.classList.toggle('active');
+}
